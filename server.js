86cad583f9e82a808cdd37fd007986e46a2cb988
Dmytro Krapyvianskyi <dimka670020040@gmail.com>
2024-06-02 14:11:04 +0300

    pragma fixing

--- server.js
+++ server.js
@@ -6,7 +6,10 @@ const port = process.env.PORT || 3000;
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Content-Type, Authorization, X-Requested-With, Pragma'
+  );
   next();
 });
 
